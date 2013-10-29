242f6a8cc0f0fa8513069ab840c2a8bfce487e14
kmcnellis <kiki15@me.com>
2013-10-28 20:08:31 -0400

    a

--- Arduino/LineFollowing2/LineFollowing2.ino
+++ Arduino/LineFollowing2/LineFollowing2.ino
@@ -46,7 +46,7 @@ int back_rightvalues[LENGTH_CACHE];
 int index = 0;
 int fl_average=0,fr_average=0,bl_average=0,br_average=0;
 int fl_total=0, fr_total=0, bl_total=0, br_total=0;
-  
+
 int t_speed=255;
 
 //function definitions
@@ -55,64 +55,77 @@ void average_func();
 void read_ldrs();
 void check_values();
 void turn(int);
+void total_change();
 
 void setup()
 {
-    Serial.begin(9600);  //Begin serial communcation
-    
-    pinMode(rMotorf,OUTPUT);
-    pinMode(rMotorb,OUTPUT);
-    pinMode(lMotorf,OUTPUT);
-    pinMode(lMotorb,OUTPUT);
-    pinMode(LED, OUTPUT);
-    Serial.println("Setup");
-    for (index = 0;index<20;index++)
-    {
-      
-      read_ldrs();
-      fl_total+=front_leftvalues[index];
-      fr_total+=front_rightvalues[index];
-      bl_total+=back_leftvalues[index];
-      br_total+=back_rightvalues[index];
+  index = 0;
+  fl_average=0;
+  fr_average=0;
+  bl_average=0;
+  br_average=0;
+  fl_total=0; 
+  fr_total=0; 
+  bl_total=0; 
+  br_total=0;
+  Serial.begin(9600);  //Begin serial communcation
 
-    }
-    Serial.println();
-    Serial.print("Front Left");
-    for (int i = 0;i<20;i++)
-    {
-      
-      Serial.print(front_leftvalues[i]);
-      Serial.print(" | ");
+  pinMode(rMotorf,OUTPUT);
+  pinMode(rMotorb,OUTPUT);
+  pinMode(lMotorf,OUTPUT);
+  pinMode(lMotorb,OUTPUT);
+  pinMode(LED, OUTPUT);
+  Serial.println("Setup");
+  for (index = 0;index<LENGTH_CACHE;index++)
+  {
+    read_ldrs();
+    fl_total+=front_leftvalues[index];
+    fr_total+=front_rightvalues[index];
+    bl_total+=back_leftvalues[index];
+    br_total+=back_rightvalues[index];
 
-    }
-    Serial.println();
-    Serial.print("Front Right");
-    for (int i = 0;i<20;i++)
-    {
-      
-      Serial.print(front_rightvalues[i]);
-      Serial.print(" | ");
+  }
 
-    }
-    Serial.println();
-    Serial.print("Back Left");
-    for (int i = 0;i<20;i++)
-    {
-      
-      Serial.print(back_leftvalues[i]);
-      Serial.print(" | ");
+  Serial.println();
+  Serial.print("Front Left");
 
-    }
-    Serial.println();
-    Serial.print("Back Right");
-    for (int i = 0;i<20;i++)
-    {
-      
-      Serial.print(back_rightvalues[i]);
-      Serial.print(" | ");
+  for (int i = 0;i<20;i++)
+  {
+    Serial.print(front_leftvalues[i]);
+    Serial.print(" | ");
+  }
 
-    }
-    Serial.println();
+  Serial.println();
+  Serial.print("Front Right");
+  for (int i = 0;i<20;i++)
+  {
+
+    Serial.print(front_rightvalues[i]);
+    Serial.print(" | ");
+
+  }
+
+  Serial.println();
+  Serial.print("Back Left");
+  for (int i = 0;i<20;i++)
+  {
+
+    Serial.print(back_leftvalues[i]);
+    Serial.print(" | ");
+
+  }
+  Serial.println();
+  Serial.print("Back Right");
+  for (int i = 0;i<20;i++)
+  {
+
+    Serial.print(back_rightvalues[i]);
+    Serial.print(" | ");
+
+  }
+  Serial.println();
+  index=0;
+  check_values();
 }
 
 void loop()
@@ -122,124 +135,79 @@ void loop()
   check_values();
   index += 1;
   index=index%20;
-  
+
+
   //Serial.println(analogRead(IR));
-  
+
   delay(200);
-}
-
-
-
-int total_func()
-{
-
-  fl_total =0;
-     
-      fr_total =0;
-      bl_total =0;
-      br_total=0;
-  for (int i=0; i <= LENGTH_CACHE; i++)
-  {
-    
-    {
-      fl_total += front_leftvalues[i];
-      //Serial.println(front_leftvalues[i]);
-      fr_total += front_rightvalues[i];
-      bl_total += back_leftvalues[i];
-      br_total += back_rightvalues[i];
-    }
-  }
-  //Serial.println();
-  //Serial.print("fl ");
-  //Serial.print(fl_total);
- // Serial.print(" | fr ");
-  //Serial.println(fr_total);
-}
-/*
-int total_change()
-{
-  int last_index=0;
-  last_index=index+1;
-  if (last_index==20)
-    last_index=0;
-  
-    
-  fl_total += front_leftvalues[index];
-  fl_total -= front_leftvalues[last_index];
-  
-  fr_total += front_rightvalues[index];
-  fr_total -= front_rightvalues[last_index];
-  
-  bl_total += back_leftvalues[index];
-  bl_total -= back_leftvalues[last_index];
-  
-  br_total += back_rightvalues[index];
-  br_total -= back_rightvalues[last_index]; 
-}
-*/
-void average_func()
-{
-  total_func();
-
-  
-  fl_average = fl_total/LENGTH_CACHE;
-  //Serial.print(fl_average);
-  //Serial.print("  |  ");
-  fr_average = fr_total/LENGTH_CACHE;
-  //Serial.println(fr_average);
-  bl_average = bl_total/LENGTH_CACHE;
-  br_average = br_total/LENGTH_CACHE;
 }
 
 void read_ldrs()
 {
   front_leftvalues[index] = analogRead(FRONTLEFT);
-  
-  
-  //Serial.println(front_leftvalues[LENGTH_CACHE-1]);
   front_rightvalues[index] = analogRead(FRONTRIGHT);
-  
-  //Serial.println(front_rightvalues[LENGTH_CACHE]);
   back_leftvalues[index] = analogRead(BACKLEFT);
   back_rightvalues[index] = analogRead(BACKRIGHT);
+  return;
 }
 
 void check_values()
 {
   average_func();
-
   if(abs(front_leftvalues[index]-fl_average)>DIFFERENCE)//this won't work for gradual/slow changes
   {
-    //Turn Right
-//    Serial.println("Right");
-    Serial.print(fr_average);
-    Serial.print(" | ");
-    Serial.print(fr_total);
-    Serial.print(" | ");
     Serial.print(fl_average);
-    Serial.print(" | ");
-    Serial.print(fl_total);
-    Serial.print(" | ");
+    Serial.print("|");
+    Serial.println(fr_average);
+
     turn(RIGHT);
-    Serial.println();
   }
   else if(abs(front_rightvalues[index] -fr_average)>DIFFERENCE)
   {
-    //Turn LEFT
-    //Serial.println("Left");
-
-    Serial.print(fr_average);
-    Serial.print(" | ");
-    Serial.print(fr_total);
-    Serial.print(" | ");
     Serial.print(fl_average);
-    Serial.print(" | ");
-    Serial.print(fl_total);
-    Serial.print(" | ");
+    Serial.print("|");
+    Serial.println(fr_average);
     turn(LEFT);
   }
   else
+  {
+    Serial.print(fl_average);
+    Serial.print("|");
+    Serial.println(fr_average);
     turn(FORWARD);
+  }
+  return;
+}
+
+void average_func()
+{
+  total_func();
+  fl_average = fl_total/LENGTH_CACHE;
+  fr_average = fr_total/LENGTH_CACHE;
+  bl_average = bl_total/LENGTH_CACHE;
+  br_average = br_total/LENGTH_CACHE;
+  return;
+}
+
+void total_func()
+{
+
+  fl_total =0;
+
+  fr_total =0;
+  bl_total =0;
+  br_total=0;
+  for (int a = 0;a<LENGTH_CACHE;a++)
+  {
+    read_ldrs();
+    fl_total+=front_leftvalues[a];
+    fr_total+=front_rightvalues[a];
+    bl_total+=back_leftvalues[a];
+    br_total+=back_rightvalues[a];
+
+  }
+
+  return;
 }
 
 void turn(int turn_signal) {
@@ -310,7 +278,7 @@ void turn(int turn_signal) {
   switch (rmotor){
   case GO:
     analogWrite(rMotorf, t_speed);
-   // digitalWrite(rMotorf, HIGH);
+    // digitalWrite(rMotorf, HIGH);
     digitalWrite(rMotorb, LOW);
     break;
 
@@ -339,7 +307,7 @@ void turn(int turn_signal) {
   switch (lmotor){
   case GO:
     analogWrite(lMotorf, t_speed);
-   // digitalWrite(lMotorf, HIGH);
+    // digitalWrite(lMotorf, HIGH);
     digitalWrite(lMotorb, LOW);
     break;
 
@@ -365,3 +333,6 @@ void turn(int turn_signal) {
     break;    
   }
 }
+
+
+
