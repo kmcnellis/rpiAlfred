a4e226a8698476587e8da9fc74cd2a4b71bbd7e0
Aaron Gunderson <airgunde@gmail.com>
2013-11-18 20:31:21 -0500

    index on master: 0d2da06 k

--- Arduino/LineFollowing_current/LineFollowing_current.ino
+++ Arduino/LineFollowing_current/LineFollowing_current.ino
@@ -1,7 +1,8 @@
-#define CLOSERIGHT A0
+#define CLOSERIGHT A2
 #define CLOSELEFT A1
-#define FARRIGHT A2
-#define FARLEFT A3
+#define FARRIGHT A3
+#define FARLEFT A0
+#define CENTER A4
 #define LENGTH_CACHE 20
 #define DIFFERENCE 15
 // rotation signals
@@ -56,20 +57,18 @@ int initFarLeft=0;
 
 #define sensorSpeed 10   
 unsigned long sensorTimer;  
-const int numSensor = 19;
+const int numSensor = LENGTH_CACHE-1;// number of values in array
 //function definitions
 
 
 boolean initial;
 boolean first;
-boolean turnMode;
 void read_ldrs();
 void average_func();
 void check_values();
 void turn(int);
 void total_change();
 void start_line();
-
 
 void setup()
 {
@@ -81,7 +80,7 @@ void setup()
   pinMode(LED, OUTPUT);
   initial=false;
   first=false;
-  turnMode=false;
+
   initCloseRight=0; 
   initCloseLeft=0;
   initFarRight=0;
@@ -122,7 +121,7 @@ void loop()
 
     }
     
-    if (index==numSensor && initial==true){
+    if (index==numSensor && initial==true){ // initial = true means it was intialized index==numSensor
       check_values();
       closeLeft_total=0; 
       closeRight_total=0; 
@@ -165,6 +164,8 @@ void read_ldrs()
   farRightValues = analogRead(FARRIGHT);
   //Serial.print(closeLeftValues);
   //Serial.print("|");
+  centerValue = analogRead(CENTER);
+  centerValue_total += centerValue;
   closeLeft_total+=closeLeftValues;
   closeRight_total+=closeRightValues;
   farLeft_total+=farLeftValues;
@@ -177,9 +178,6 @@ void check_values()
   average_func();
   int diffLeft=abs(closeLeft_average-initCloseLeft);
   int diffRight=abs(closeRight_average -initCloseRight);
-  int diffLeftFar=abs(farLeft_average-initFarLeft);
-  int diffRightFar=abs(farRight_average -initFarRight);
-/*
   Serial.print(diffLeft);
   Serial.print("|");
   Serial.print(diffRight);
@@ -191,35 +189,22 @@ void check_values()
   Serial.print(initCloseLeft);
   Serial.print("|");
   Serial.println(initCloseRight);
-*/
-  if(diffLeftFar>DIFFERENCE)
-  {
-    turnMode=true;
-    turn(F_LEFT);
-  }
-  if(diffRightFar>DIFFERENCE)
-  {
-    turnMode=true;
-    turn(F_RIGHT);
-
-  }
+  
   if(diffLeft>diffRight && diffLeft>DIFFERENCE)//this won't work for gradual/slow changes
   {
-    if (turnMode)
-    {
-      turn(F_LEFT);
-    }
-    else
-    {  
-      turn(F_RIGHT);
-    }
+
+
+    turn(RIGHT);
   }
   else if(diffRight>diffLeft && diffRight>DIFFERENCE)
   {
+   
     turn(LEFT);
   }
   else
-  { 
+  {
+
+    
     turn(FORWARD);
   }
   return;
@@ -359,6 +344,7 @@ void turn(int turn_signal) {
     break;    
   }
 }
+
 
 
 
