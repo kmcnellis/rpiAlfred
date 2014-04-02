a4b4bbbe1024ddeaa6232b7017a68fe1d6a2f80c
Aaron <airgunde@gmail.com>
2014-04-01 23:10:19 -0400

    Restructured code

--- gen/com/example/alfred_bluetooth_controller/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.alfred_bluetooth_controller;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
